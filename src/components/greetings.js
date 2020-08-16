4d9cbbe277a85af10f6797380cf6e825114df1c1
AB8763 <48782966+AB8763@users.noreply.github.com>
2020-08-16 23:03:57 +0200

    Try to fix it, but failed

--- src/components/greetings.js
+++ src/components/greetings.js
@@ -2,23 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Greeting extends React Component{
-  getGreeting(){
-    var h = new Date().getHours();
-    var name = "Jerry";
-    if(h<12 && h>4){
-      text = "Good morning, ";
-    } else if(h<17 && h>12){
-      text = "Good afternoon, ";
-    }else {
-      text = "Good evening, ";
-    }
-    return gtext+name+".";
-  }
+
 
   render(){
     return(
-      <p>{this.getGreeting()}</p>
+      const greet = () =>{
+        var h = new Date().getHours();
+        var name = "Jerry";
+        if(h<12 && h>4){
+          text = "Good morning, ";
+        } else if(h<17 && h>12){
+          text = "Good afternoon, ";
+        }else {
+          text = "Good evening, ";
+        }
+        return gtext+name+".";
+      }
+
     );
   }
 }
-export default classnamn;
+export default Greeting;
