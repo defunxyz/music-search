89e4f94ee328bf9d802c4901f74e6e9df5d19150
sismasodit <fisnikh13@gmail.com>
2020-08-17 09:11:28 +0200

    Updated Greeting.js

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 
-class Greeting extends React.Component {
+class Greeting extends Component {
   constructor(props) {
     super(props)
     this.state = {
