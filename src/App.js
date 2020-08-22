ce2b62168ff450c6ec11c570daf01add0796f96e
sismasodit <fisnikh13@gmail.com>
2020-08-22 17:25:10 +0200

    API Facilitator Major Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import { authSpotifyAPI } from './api';
+import { authSpotifyAPI, authNapsterAPI, getSpotifyToken } from './api';
 import './App.css';
 
 class App extends React.Component {
-  
-  async componentDidMount() {
-    authSpotifyAPI();
+  constructor(prop){
+    super(prop);
+    this.state = {
+      spotify_token: {},
+      error: false, 
+      errorMessage: ""
+    };
+  }
+
+  componentDidMount(){
+    if (Object.keys(this.state.spotify_token).length === 0) {
+      this.authSpotify();
+      this.authNapster();
+    }
+  }
+
+  authNapster = async () => {
+    authNapsterAPI();
+  }
+
+  authSpotify = async () => {
+    let success = false;
+    try{
+      success = await authSpotifyAPI();
+    } catch(e) {
+      if(!this.state.error) {
+        this.setState({
+          error: true, 
+          errorMessage: e.message
+        });
+      }
+    }
+
+    if(success) {
+      this.setState({
+      spotify_token: getSpotifyToken()
+      });
+
+      // Next line to be removed once localStorage is implemented
+      console.log(this.state.spotify_token);
+    }
   }
 
   render() {
     return (
       <>
-      API Facilitator
+      <h1>API Facilitator</h1>
+
+      <h4>Spotify Auth Token:</h4>
+      <p><strong>Token Type: </strong>{this.state.spotify_token.token_type }</p>
+      <p><strong>Expires In: </strong> {this.state.spotify_token.expires_in }</p>
+      <p><strong>Access Token: </strong>{this.state.spotify_token.access_token }</p>
       </>
     );
   }
--- src/api/index.js
+++ src/api/index.js
@@ -16,19 +16,50 @@ var querystring = require('querystring');
 
 // API Keys
 const NAPSTER_API_KEY = 'MDA1YWVlOTAtYTgxYy00YzhlLTg0ZDAtYWNiM2ZmNWRjMGFm';
+const NAPSTER_API_KEY_SECRET = 'YTg1YTQ0MWYtZjQ5Mi00NjAzLWEwOWUtN2UyYjI3YjE2NzNk';
+
 const SPOTIFY_CLIENT_ID = 'c22cebab4f14406b9d20ce55d8059b26';
 const SPOTIFY_CLIENT_SECRET = 'a7f683d12dae40f5a8a6a162cc408bbe';
 
-// Endpoints
+// Endpoints ?client_id=${APIKEY}&response_type=code
+const Napster_Auth_Endpoint = `https://api.napster.com/oauth/access_token`;
+
 const Spotify_Auth_Endpoint = `https://accounts.spotify.com/api/token`;
 const Spotify_Search_Endpoint = (term, type) => `https://api.spotify.com/v1/search?q=${term}&type=${type}`;
 const Napster_Search_Endpoint = (key, term) => `http://api.napster.com/v2.2/search/verbose?apikey=${key}&query=${term}`;
 
-// Spotify Token
+// Tokens
 let spotify_token = {};
+//let napster_token = {};
 
-export const authSpotifyAPI = () => {
+// This function below is NOT finished yet.
+export const authNapsterAPI = async () => {
     axios({
+        url: Napster_Auth_Endpoint,
+        method: 'post',
+        params: {
+            client_id: NAPSTER_API_KEY,
+            client_secret: NAPSTER_API_KEY_SECRET
+        },
+        headers: {
+            'Accept':'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            // 'Authorization': 'Bearer ' + (new Buffer(NAPSTER_API_KEY + ':' + NAPSTER_API_KEY_SECRET).toString('base64'))
+        }
+    }).then(function(response) {
+        console.log(response);
+    }).catch(function(error){
+        console.log(error);
+    });
+};
+
+/**
+ * Asynchronous function that retrieves a Spotify Token for current session.
+ *
+ * @returns {Boolean} Returns true if the request was successful, else false along with error message
+ */
+export const authSpotifyAPI = async () => {
+    return axios({
         url: Spotify_Auth_Endpoint,
         method: 'post',
         headers: {
@@ -36,25 +67,32 @@ export const authSpotifyAPI = () => {
           'Content-Type': 'application/x-www-form-urlencoded',
           'Authorization': 'Basic ' + (new Buffer(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET).toString('base64'))
         },
-        data: querystring.stringify({ grant_type: 'client_credentials' }),
+        data: querystring.stringify({ grant_type: 'client_credentials'}),
         auth: {
           username: SPOTIFY_CLIENT_ID,
           password: SPOTIFY_CLIENT_SECRET
         }
     }).then(function(response) {
         spotify_token = response.data;
-        console.log(spotify_token);
+        return true;
     }).catch(function(error) {
-        console.log(error);
+        return {status: false, error: error};
     });
 };
 
-export const searchSpotifyAPI = (term, type) => {
+export const searchNapsterAPI = async (term) => {
+    term = encodeURIComponent(term);
+    axios.get(Napster_Search_Endpoint(NAPSTER_API_KEY, term));
+};
+
+export const searchSpotifyAPI = async (term, type) => {
     term = encodeURIComponent(term);
     axios.get(Spotify_Search_Endpoint(term, type));
 };
 
-export const searchNapsterAPI = (term) => {
-    term = encodeURIComponent(term);
-    axios.get(Napster_Search_Endpoint(NAPSTER_API_KEY, term));
-};
+/**
+ * A function that returns Spotify Token information in current session.
+ * 
+ * @returns {Object} Spotify Token Data
+ */
+export const getSpotifyToken = () => { return spotify_token; };
