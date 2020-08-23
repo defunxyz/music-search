3a0b8422014a427d6bd102e8a3a5db69db6ea602
sismasodit <fisnikh13@gmail.com>
2020-08-23 17:22:11 +0200

    Updated Hooks index.js

---
+++ src/hooks/index.js
@@ -0,0 +1,16 @@
+/**
+ * Hooks Components
+ * https://reactjs.org/docs/hooks-intro.html 
+ * 
+ * @file hooks/index.js
+ * @author 
+ * @copyright 2020 
+ */
+
+import { useEffect } from 'react';
+
+export const useMobileDetect = () => {
+    useEffect(() => {
+        // Write code here
+    }, []);
+};
