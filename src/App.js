8dd9021feca334407ec066434a290434165c13bb
sismasodit <fisnikh13@gmail.com>
2020-11-09 18:35:11 +0100

    Skeleton Search Component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink
-} from 'react-router-dom';
-import { Home, About, Music, Settings } from './views';
+import Search from './components/search';
 
 export default class App extends React.Component {
   render() {
     return(
-      <Router>
-        <nav>
-          <ul>
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink exact to="/music">Music</NavLink></li>
-            <li><NavLink exact to="/about">About</NavLink></li>
-            <li><NavLink exact to="/settings">Settings</NavLink></li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/settings">
-            <Settings />
-          </Route>
-          <Router path="/about">
-            <About />
-          </Router>
-          <Route path="/music">
-            <Music />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+     <Search />
     );
   }
 };
---
+++ src/components/search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Search extends React.Component {
+    constructor() {
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+};
