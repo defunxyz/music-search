4dc378810c2228e553820e3668d9bafe75632c5b
sismasodit <fisnikh13@gmail.com>
2020-12-05 20:22:14 +0100

    Minor

--- src/utility.js
+++ src/utility.js
@@ -2,16 +2,15 @@ export default function convertNumber(number){
 		var nbr=number.toString();
 		var breakPoint=nbr.length%3;
 		var result="";		
-		if((breakPoint==0)&&(nbr.length>=6)){
+		if((breakPoint===0)&&(nbr.length>=6)){
 			breakPoint+=3;
 		}
 		for(var j=0; j<nbr.length; j++){
-				if((j==breakPoint)&&(breakPoint!=0)){	
+				if((j===breakPoint)&&(breakPoint!==0)){	
 					result+=",";						
 					breakPoint+=3;
 				} 
 				result+=nbr[j];
 		}
-		console.log(result);
 		return result;
 }
