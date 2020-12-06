d96e7c35b17cdbd95a77518645469930b53c8474
sismasodit <fisnikh13@gmail.com>
2020-12-06 20:10:28 +0100

    Updated

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Scrollbars } from 'react-custom-scrollbars';
+import dateFormat from 'dateformat';
 import "./index.css";
 
 const Album = (props) => {
@@ -16,14 +17,18 @@ const Album = (props) => {
             <img src={data.images[1].url} alt="" />
         </div>
         <h1>{data.name}</h1>
-        <div className="artist-stats">
-            <div className="stat">
-                <span className="number">{data.popularity}</span>
-                <span className="infom">Popularity</span>
+        <div className="clearfix">
+            <div className="album-info lfloat">
+                <span id="artist" className="artist">{data.artists[0].name}</span>
+                <span>{dateFormat(data.release_date, "d mmmm yyyy")}</span>
+                <span>{data.label}</span>
             </div>
-        </div>
-        <div className="album-info">
-            
+            <div className="album-stat rfloat">
+                <div className="stat">
+                    <span className="number">{data.popularity}</span>
+                    <span className="infom">Popularity</span>
+                </div>
+            </div>
         </div>
         {result !== undefined && <div className="lyrics">
             <Scrollbars style={{ height: 200 }}>
